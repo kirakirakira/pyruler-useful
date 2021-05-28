a0ec371ce046dda9bce0df6febeee02528f579c3
Kira Hartlage <kira.hartlage@gmail.com>
2021-05-28 16:38:24 -0400

    add sound meter fun with lights!

---
+++ soundmeter-fun.py
@@ -0,0 +1,61 @@
+"""This example uses the sound sensor, located next to the picture of the ear on your board, to
+light up the NeoPixels as a sound meter. Try talking to your Circuit Playground or clapping, etc,
+to see the NeoPixels light up!"""
+import array
+import math
+import audiobusio
+import board
+from adafruit_circuitplayground import cp
+
+cp.pixels.brightness = 0.03
+
+def constrain(value, floor, ceiling):
+    return max(floor, min(value, ceiling))
+
+
+def log_scale(input_value, input_min, input_max, output_min, output_max):
+    normalized_input_value = (input_value - input_min) / (input_max - input_min)
+    return output_min + math.pow(normalized_input_value, 0.630957) * (
+        output_max - output_min
+    )
+
+
+def normalized_rms(values):
+    minbuf = int(sum(values) / len(values))
+    return math.sqrt(
+        sum(float(sample - minbuf) * (sample - minbuf) for sample in values)
+        / len(values)
+    )
+
+input_floor = cp.sound_level + 10
+
+# Lower number means more sensitive - more LEDs will light up with less sound.
+sensitivity = 500
+input_ceiling = input_floor + sensitivity
+
+peak = 0
+while True:
+    magnitude = cp.sound_level
+
+    print((magnitude,))  # Printed as a tuple for the Mu plotter.
+
+    c = log_scale(
+        constrain(magnitude, input_floor, input_ceiling),
+        input_floor,
+        input_ceiling,
+        0,
+        10,
+    )
+
+    cp.pixels.fill((0, 0, 0))
+    for i in range(10):
+        if i < c:
+            cp.pixels[i] = (i * (255 // 10), 50, 0)
+        if c >= peak:
+            peak = min(c, 10 - 1)
+        elif peak > 0:
+            peak = peak - 1
+        if peak > 0:
+            cp.pixels[int(peak)] = (80, 0, 255)
+    cp.pixels.show()
+
